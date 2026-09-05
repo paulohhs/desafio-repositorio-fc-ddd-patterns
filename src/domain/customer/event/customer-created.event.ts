0bff6aac47d848c05e104c5169dbfe34b3dbfd46
paulohhs <paulohhonoratos@gmail.com>
2026-09-04 22:03:07 -0300

    custome event(src/domain/customer/event/customer-created.event.ts): implementa CustomCreatedEvent.

---
+++ src/domain/customer/event/customer-created.event.ts
@@ -0,0 +1,16 @@
+import EventInterface from "../../@shared/event/event.interface";
+
+export interface CustomerCreatedEventData {
+  id: string;
+  name: string;
+}
+
+export default class CustomerCreatedEvent implements EventInterface {
+    dataTimeOccurred: Date;
+    eventData: CustomerCreatedEventData;
+
+    constructor(eventData: CustomerCreatedEventData) {
+        this.dataTimeOccurred = new Date();
+        this.eventData = eventData;
+    }
+}
